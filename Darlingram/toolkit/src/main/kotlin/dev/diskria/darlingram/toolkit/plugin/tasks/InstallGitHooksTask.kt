08131b9372fa598aae8b64a29ac6447ea7188d1e
diskria <diskreee@gmail.com>
2025-07-06 00:36:48 +0400

    build: 🧪 add Git commit-msg hook and Gradle task to install hooks
    - Added commit-msg hook to validate commit messages
    - Toolkit module now includes a Gradle task to set up Git hooks automatically

---
+++ Darlingram/toolkit/src/main/kotlin/dev/diskria/darlingram/toolkit/plugin/tasks/InstallGitHooksTask.kt
@@ -0,0 +1,20 @@
+package dev.diskria.darlingram.toolkit.plugin.tasks
+
+import dev.diskria.darlingram.tools.kotlin.utils.Shell
+import java.io.File
+
+@Suppress("unused")
+abstract class InstallGitHooksTask : GradleToolkitTask(
+    "Install git hooks"
+) {
+    override fun runTask() {
+        val sourceDirectory = directories.getGitHooks()
+        val gitHooksDirectoryPath = Shell(directories.getUpstreamRoot()).runWithOutput(
+            "git rev-parse --git-path hooks"
+        )
+        val targetDirectory = File(gitHooksDirectoryPath)
+        sourceDirectory.listFiles()?.forEach { sourceFile ->
+            sourceFile.copyTo(targetDirectory.resolve(sourceFile.name), overwrite = true)
+        }
+    }
+}
