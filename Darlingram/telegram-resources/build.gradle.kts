d35a939370e4e48b7b6d28a6fc3be47e33397037
diskria <diskreee@gmail.com>
2025-07-06 00:51:49 +0400

    build: 🔗 add wrapper module for Telegram res and assets

---
+++ Darlingram/telegram-resources/build.gradle.kts
@@ -0,0 +1,50 @@
+import com.android.SdkConstants
+import dev.diskria.darlingram.toolkit.extensions.directories
+import dev.diskria.darlingram.toolkit.extensions.value
+import dev.diskria.darlingram.tools.kotlin.extensions.appendPackageName
+import dev.diskria.darlingram.tools.kotlin.extensions.toPackageName
+
+plugins {
+    alias(config.plugins.android.library)
+    alias(tools.plugins.toolkit)
+}
+
+val packageName: String by rootProject.extra
+val upstreamResourcesWrapperModule: String by rootProject.extra
+
+android {
+    namespace = packageName.appendPackageName(upstreamResourcesWrapperModule.toPackageName())
+
+    compileSdk = config.versions.compile.sdk.value()
+
+    defaultConfig {
+        minSdk = config.versions.min.sdk.value()
+    }
+
+    sourceSets[SdkConstants.FD_MAIN].apply {
+        project
+            .directories()
+            .getUpstreamLibraryModule()
+            .resolve(SdkConstants.FD_SOURCES)
+            .resolve(SdkConstants.FD_MAIN)
+            .let { mainDirectory ->
+                mapOf(
+                    assets to SdkConstants.FD_ASSETS,
+                    res to SdkConstants.FD_RES
+                ).forEach { target, source ->
+                    target.srcDir(file(mainDirectory.resolve(source)))
+                }
+            }
+    }
+
+    lint {
+        disable += listOf(
+            "MissingTranslation",
+            "ExtraTranslation"
+        )
+    }
+}
+
+dependencies {
+    implementation(libs.appcompat)
+}
