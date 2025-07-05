42866b3c09349fd2c57e57a76b3caab4c67bf794
diskria <diskreee@gmail.com>
2025-07-06 00:40:59 +0400

    dx: 📲 add Gradle task to collect APKs in /apk with max files setting
    - Created Gradle task to copy built APKs into /apk directory at the project root
    - Automatically limits the number of stored APKs (old files are deleted)
    - Simplifies access to latest builds across variants

---
+++ Darlingram/toolkit/src/main/kotlin/dev/diskria/darlingram/toolkit/plugin/tasks/ExportAPKTask.kt
@@ -0,0 +1,59 @@
+package dev.diskria.darlingram.toolkit.plugin.tasks
+
+import dev.diskria.darlingram.Metadata
+import dev.diskria.darlingram.toolkit.extensions.getForkLocalProperty
+import dev.diskria.darlingram.toolkit.extensions.isFork
+import dev.diskria.darlingram.tools.kotlin.extensions.takeIfExceeds
+import dev.diskria.darlingram.tools.kotlin.utils.Constants
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+@Suppress("unused")
+abstract class ExportAPKTask : GradleToolkitTask(
+    "Copies the latest built APK into the repo root"
+) {
+    override fun runTask() {
+        val apkFile = buildDir()
+            .walkTopDown()
+            .filter { it.isFile && it.extension == Constants.File.Extension.APK }
+            .maxByOrNull { it.lastModified() }
+            ?: error("APK not found")
+
+        val buildType = apkFile.parentFile.name
+        val sdfDate = SimpleDateFormat("yyyy-MM-dd", Locale.US)
+        val sdfTime = SimpleDateFormat("HH:mm:ss", Locale.US)
+
+        val now = Date()
+        val date = sdfDate.format(now)
+        val time = sdfTime.format(now)
+
+        val apkName = listOf(
+            if (project.isFork()) Metadata.FORK_NAME else Metadata.UPSTREAM_NAME,
+            buildType,
+            date,
+            time
+        ).joinToString(Constants.Symbol.UNDERSCORE) +
+                Constants.Symbol.DOT +
+                Constants.File.Extension.APK
+        val outputDirectory = directories.getAPK(project.isFork())
+        outputDirectory.mkdirs()
+
+        val filesLimit = project
+            .getForkLocalProperty("APK_FILES_LIMIT")
+            ?.toInt()
+            ?: DEFAULT_FILES_LIMIT
+        apkFile.copyTo(outputDirectory.resolve(apkName), overwrite = true)
+
+        val apkFiles = outputDirectory
+            .listFiles { file -> file.isFile && file.extension == Constants.File.Extension.APK }
+            ?.sortedBy { it.lastModified() }
+            .orEmpty()
+
+        apkFiles.takeIfExceeds(filesLimit)?.forEach { it.delete() }
+    }
+
+    companion object {
+        private const val DEFAULT_FILES_LIMIT = 5
+    }
+}
