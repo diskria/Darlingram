05f471a195f435c4e2334007d689d298ebb191c6
diskria <diskreee@gmail.com>
2025-07-05 23:39:45 +0400

    dx: 💥 introduce extensible gradle settings setup

---
+++ gradle-configuration/settings/main.gradle.kts
@@ -0,0 +1,11 @@
+private val GRADLE_KTS_EXTENSION = ".gradle.kts"
+
+file(".")
+    .listFiles { file ->
+        file.isFile &&
+                file.name.endsWith(GRADLE_KTS_EXTENSION) &&
+                file.name != "main" + GRADLE_KTS_EXTENSION
+    }
+    ?.forEach { gradleSettings ->
+        apply(from = gradleSettings)
+    }
